483c4b4506271d337180026f8cd9c724aea2ecd5
Ivan-Killer <ivan.stefanov.513@gmail.com>
2018-06-27 16:06:56 +0300

    LED update

--- ivan/ledControl.c
+++ ivan/ledControl.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include "pinout.h"
 
+enum led_enum{LED1, LED2, LED3, LED4};
+
 int main(int argc, char const *argv[])
 {
+	enum led_enum leds;
 	wiringPiSetup();
 	pinMode(LED1,OUTPUT);
 	pinMode(LED2,OUTPUT);
@@ -14,6 +17,30 @@ int main(int argc, char const *argv[])
 	printf("2 -> %s\n", argv[2]);
 	printf("3 -> %s\n", argv[3]);
 	printf("4 -> %s\n", argv[4]);
+
+	for(int cnt = 1 ; cnt < 5 ; cnt++)
+	{
+		leds = cnt;
+
+		if (strcmp(argv[1], "0") == 0) 
+		{
+			digitalWrite(leds, LOW);
+		} 
+		else if (strcmp(argv[1], "1") == 0)
+		{
+			digitalWrite(leds, HIGH);
+		}
+		else if (strcmp(argv[1], "2") == 0)
+		{
+			//don't change it
+		}
+		else
+		{
+			printf("Somethink must be really fucked up!\n");
+		}
+
+		printf("%d -> %d\n", cnt, leds);
+	}
 
 	return 0;
 }
